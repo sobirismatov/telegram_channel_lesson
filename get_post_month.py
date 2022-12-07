069770ad3d90b2214f796b09eb1e7c8b584a3ba6
sobirismatov <name@uzcoder96@gmail.com>
2022-12-07 15:15:18 +0500

    02

--- get_post_month.py
+++ get_post_month.py
@@ -1,6 +1,6 @@
 from read_data import fromJson
-
-
+data=fromJson("data/result.json")
+from datetime import datetime
 def get_post_month(data:dict,month:int)->int:
     """
     Return the number of posts for a given month
@@ -12,5 +12,11 @@ def get_post_month(data:dict,month:int)->int:
     Returns: 
         int: the number of posts for the given month
     """
-    
-    return
+    data=data["messages"]
+    a=0
+    for i in data:
+        if i['type']=='message':
+            a+=int(i['date'][5:7]) == month
+    return a
+print(get_post_month(data,5))
+        
